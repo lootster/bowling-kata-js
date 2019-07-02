04a8bd1d2316a7d288a84d47dbcb049eb4e9246b
Sheldon <sheldon.lim@online-pajak.com>
2019-07-02 10:01:41 +0800

    Write second test

--- test/bowlingGame.test.js
+++ test/bowlingGame.test.js
@@ -1,11 +1,18 @@
 const BowlingGame = require("../src/bowlingGame");
 
-describe('Bowling Game Test', () => {
-    it('should test for gutter game', () => {
-        let game = new BowlingGame();
-        for(let i=0; i < 20; i++) {
-            game.roll(0);
-        }
-        expect(game.score()).toEqual(0);
-    });
+describe("Bowling Game Test", () => {
+  it("should test for gutter game", () => {
+    let game = new BowlingGame();
+    for (let i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(0);
+  });
+  it("should return 20 score after a game", () => {
+    let game = new BowlingGame();
+    for (let i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toEqual(20);
+  });
 });
