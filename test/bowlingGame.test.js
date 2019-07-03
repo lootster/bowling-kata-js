e5bab69b50fdaeaee762f9a2120750fcc9589e26
Sheldon <sheldon.lim@online-pajak.com>
2019-07-03 09:12:19 +0800

    Write third test

--- test/bowlingGame.test.js
+++ test/bowlingGame.test.js
@@ -14,9 +14,16 @@ describe("Bowling Game Test", () => {
     rollMany(20, 0)
     expect(game.score()).toEqual(0);
   });
-  
+
   it("should return 20 score after a game", () => {
     rollMany(20, 1)
     expect(game.score()).toEqual(20);
+  });
+  it('should handle one Spare in a game', () => {
+    game.roll(5);
+    game.roll(5); // Spare
+    game.roll(3);
+    rollMany(17, 0);
+    expect(game.score()).toEqual(16);
   });
 });
