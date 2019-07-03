eee91ed912b06b43b32676afc0cde2112e6d4cd3
Sheldon <sheldon.lim@online-pajak.com>
2019-07-03 09:21:05 +0800

    Refactor

--- test/bowlingGame.test.js
+++ test/bowlingGame.test.js
@@ -1,25 +1,28 @@
 const BowlingGame = require("../src/bowlingGame");
 
 describe("Bowling Game Test", () => {
+  let game;
 
-  let game = new BowlingGame();
+  beforeEach(() => {
+    game = new BowlingGame();
+  });
 
   function rollMany(nTimesOfRoll, pins) {
     for (let i = 0; i < nTimesOfRoll; i++) {
-      game.roll(pins)
+      game.roll(pins);
     }
   }
 
   it("should test for gutter game", () => {
-    rollMany(20, 0)
+    rollMany(20, 0);
     expect(game.score()).toEqual(0);
   });
 
   it("should return 20 score after a game", () => {
-    rollMany(20, 1)
+    rollMany(20, 1);
     expect(game.score()).toEqual(20);
   });
-  it('should handle one Spare in a game', () => {
+  it("should handle one Spare in a game", () => {
     game.roll(5);
     game.roll(5); // Spare
     game.roll(3);
