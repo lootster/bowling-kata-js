699535e86b7d72870fdb5c306f0a334795ed2e0f
Sheldon <sheldon.lim@online-pajak.com>
2019-07-04 14:23:04 +0800

    introduce variable "frameIndex"

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -6,15 +6,19 @@ class BowlingGame {
     this.rolls.push(pins);
   }
   score() {
+    let rolls = this.rolls;
     let score = 0;
-    let i = 0;
+    let frameIndex = 0;
     for (let frame = 0; frame < 10; frame++) {
-      if (this.rolls[i] + this.rolls[i + 1] === 10) {
-        score += 10 + this.rolls[i + 2];
-        i += 2;
-      } else {
-        score += this.rolls[i] + this.rolls[i + 1];
-        i += 2;
+      if (rolls[frameIndex] + rolls[frameIndex + 1] === 10) 
+      {
+        score += 10 + rolls[frameIndex + 2];
+        frameIndex += 2;
+      } 
+      else 
+      {
+        score += rolls[frameIndex] + rolls[frameIndex + 1];
+        frameIndex += 2;
       }
     }
     return score;
